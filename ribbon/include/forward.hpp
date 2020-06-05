daeee2bbaadf34f35503156bbd29ea7506b09a73
zl-su <su-zl@seu.edu.cn>
2020-06-05 21:26:18 +0800

    Add forward header.

---
+++ ribbon/include/forward.hpp
@@ -0,0 +1,10 @@
+#pragma once
+class QLabel;
+class QFrame;
+class RibbonButtonGroup;
+class RibbonTabContent;
+class RibbonWidget;
+namespace Ui {
+	class RibbonTabContent;
+	class RibbonButtonGroup;
+}
